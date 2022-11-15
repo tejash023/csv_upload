5a14d26593496eae929593e3402720e83a2d2ace
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-15 13:22:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '8080';
+const db = require('./config/mongoose');
+
+
+//starting the server
+app.listen(port, function(err){
+  if(err){console.log("Error in connecting with the server", err); return}
+  console.log(`Sucessfully connected to the server at port ${port}`);
+})
