a6c06afde4e5c8e949679e63dd1f2827a0c7174f
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-19 22:02:44 +0530

    added papaparse to parse the CSV to JSON

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,81 @@
 const CSVFile = require('../models/mongoose');
 const fs = require('fs');
 const path = require('path');
-const parse = require('papaparse');
+const papa = require('papaparse');
 
 module.exports.homePage = (req, res) => {
   res.render('home');
  
 }
 
-module.exports.uploadCSV = (req, res) => {
-  try{
+// module.exports.uploadCSV = (req, res) => {
+//   try{
       
-    //let user = await User.findById(req.params.id);
-    CSVFile.uploadedCSV(req, res, function(err){
-      if(err){console.log('********** Multer Error')};
+    
+//     CSVFile.uploadedCSV(req, res, function(err){
+//       if(err){console.log('********** Multer Error')};
 
-      console.log(req.file);
+//       console.log(req.file);
 
-      if(req.file){
+//       if(req.file){
 
-        //fs.unlinkSync(path.join(__dirname, '..', CSVFile.csvPath + '/' + req.file.filename));
-        //this is for saving the path of the uploaded file 
-        //user.csv = User.csvPath + '/' + req.file.filename;
+//         //fs.unlinkSync(path.join(__dirname, '..', CSVFile.csvPath + '/' + req.file.filename));
+//         //this is for saving the path of the uploaded file 
+//         //user.csv = User.csvPath + '/' + req.file.filename;
+//       }
+      
+//       return res.redirect('back');
+//     })
+//   }catch{
+
+//   }
+// }
+
+module.exports.uploadFile = (req, res) => {
+
+  CSVFile.uploadedCSV(req, res, async function(err){
+    try{
+
+      let csvFile = await CSVFile.findOne({name:req.file.originalname});
+      if(csvFile){
+        console.log('csv already exists');
+        return res.redirect('back');
       }
-      
-      return res.redirect('back');
-    })
-  }catch{
 
-  }
+      // let conversedFile = papa.parse(req.file.buffer.toString(),{
+      //   delimiter:';',
+      //   escapeChar:'\\',
+      //   header: false,
+
+      //   error:function(error){
+      //     process.send(['search-failed', 'process']);
+      //     console.log(error);
+      //   }
+      // });
+
+      const CSVFileUP = req.file.path;
+      const csvData = fs.readFileSync(CSVFileUP, 'utf8');
+
+      const conversedFile = papa.parse(csvData, { 
+        header: true 
+      
+      });
+
+      console.log(conversedFile);
+
+      if(req.file && req.file.mimetype == 'text/csv'){
+        let csvFile = CSVFile.create({
+          name: req.file.originalname,
+          file: conversedFile.data
+        });
+
+        return res.redirect('back');
+      }
+
+
+    }catch(err){
+      console.log("error", err);
+      
+    }
+  })
 }
