8956da90546a41dd3cc347312e40db8767088933
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-19 20:44:38 +0530

    added models

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+
+//defining Schema for CSV data storage
+const csvSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  file:{
+    type: Array
+  }
+},{
+  timestamps: true
+});
+
+const CSVFile = mongoose.model('CSVFile', csvSchema)
+module.exports =  CSVFile;
