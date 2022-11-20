3cfc791a6d09293ce71648368cccd9cf4c03488d
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-20 12:34:58 +0530

    styling homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+const toggleMode = document.querySelector('.fa-moon');
+
+toggleMode.addEventListener('click', function(){
+  console.log('Hi');
+})
