9f0b1d6f92e83d8ae83820ee2057a6829a8712c3
Tejashwa Rajvardhan <keshu023@gmail.com>
2022-11-20 15:43:49 +0530

    redirected routes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
-
-const toggleMode = document.querySelector('.fa-moon');
-
-toggleMode.addEventListener('click', function(){
-  console.log('Hi');
-})
